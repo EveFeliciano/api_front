8f3ec2c755227301f558c842b8419dc8178a6ad5
EveFeliciano <evellynsanfeliciano@gmail.com>
2025-09-09 12:16:30 -0300

    front

--- etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
+++ etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
@@ -3,60 +3,44 @@ import Lixeira from '../../assets/lixeira.png'
 
 function Home() {
   const usuarios = [
-  {
-    id: 'jhfdjhjsdhfjh',
-    nome: 'Teste',
-    idade: 30,
-    email: 'teste@email.com'
-  },
-  {
-    id: '1374878ehhwe',
-    nome: 'Aline',
-    idade: 28,
-    email: 'aline@email.com'
-  }
-]
+    {
+      id: 'jhfdjhjsdhfjh',
+      nome: 'Teste',
+      idade: 30,
+      email: 'teste@email.com'
+    },
+    {
+      id: '1374878ehhwe',
+      nome: 'Aline',
+      idade: 28,
+      email: 'aline@email.com'
+    }
+  ]
 
   return (
     <div className='container'>
-  <form>
-    <h1>Cadastro de Usuários</h1>
-    <input name='nome' type='text' />
-    <input name='idade' type='number' />
-    <input name='email' type='email' />
-    <button type='button'>Cadastrar</button>
-  </form>
-  {usuarios.map(usuario => (
-    <div key={usuario.id} className='card'>
-    
-    <div>
-      <p>Nome: {usuario.nome}</p>
-      <p>Idade: {usuario.idade}</p>
-      <p>Email: {usuario.email}</p>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name='nome' type='text' placeholder='Nome' />
+        <input name='idade' type='number' placeholder='Idade' />
+        <input name='email' type='email' placeholder='Email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {usuarios.map(usuario => (
+        <div key={usuario.id} className='card'>
+          <div className='card-info'>
+            <p>Nome: <span>{usuario.nome}</span></p>
+            <p>Idade: <span>{usuario.idade}</span></p>
+            <p>Email: <span>{usuario.email}</span></p>
+          </div>
+          <button className='delete-btn'>
+            <img src={Lixeira} alt="Excluir" />
+          </button>
+        </div>
+      ))}
     </div>
-    <button>
-      <img src={Lixeira} />
-    </button>
-  </div>
-))}
-
-
-  <div>
-    <div>
-      <p>Nome: </p>
-      <p>Idade: </p>
-      <p>Email: </p>
-    </div>
-    <button>
-       <img src={Lixeira}/>
-    </button>
-  </div>
-</div>
-
-    
-    
   )
 }
-
 
 export default Home
