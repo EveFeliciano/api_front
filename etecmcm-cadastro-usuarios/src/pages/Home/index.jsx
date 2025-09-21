a86e397d5ea975893ec843a3dc8d886ccff7b21d
EveFeliciano <evellynsanfeliciano@gmail.com>
2025-09-21 00:06:10 -0300

    aula 18

--- etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
+++ etecmcm-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,41 +1,60 @@
 import './style.css'
 import Lixeira from '../../assets/lixeira.png'
 import api from '../../services/api'
-import { useEffect  } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 function Home() {
-  
-  let usuarios = []
+  const [usuarios, setUsuarios] = useState([])
+  //let usuarios = []
 
-  async function getUsuarios(){
-    usuarios = await api.get('/cadastro')
+  const inputNome = useRef()
+  const inputEmail = useRef()
+  const inputIdade = useRef()
+
+  async function getUsuarios() {
+    const usuariosDaApi = await api.get('/cadastro')
+    //usuarios = usuariosDaApi.get('/cadastro')
+    setUsuarios(usuariosDaApi.data)
     console.log(usuarios)
   }
 
-  useEffect(()=>{
+  async function createUsuarios() {
+    await api.post('/cadastro',{
+      email: inputEmail.current.value,
+      nome: inputNome.current.value,
+      idade: inputIdade.current.value
+    })
     getUsuarios()
-  },[])
+  }
 
+  async function deleteUsuarios(id) {
+    await api.delete(`/cadastro/${id}`) 
+  }
+
+  useEffect(() =>{
+    getUsuarios()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
 
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text' placeholder='Nome' />
-        <input name='idade' type='number' placeholder='Idade' />
-        <input name='email' type='email' placeholder='Email' />
-        <button type='button'>Cadastrar</button>
+        <input placeholder='Digite seu Nome' name="nome" type="text" ref={inputNome}/>
+        <input placeholder='Digite sua Idade' name="idade" type="number" ref={inputIdade}/>
+        <input placeholder='Digite seu Email' name="email" type="email" ref={inputEmail}/>
+        <button type='button' onClick={createUsuarios}>Cadastrar</button>
       </form>
 
       {usuarios.map(usuario => (
         <div key={usuario.id} className='card'>
-          <div className='card-info'>
-            <p>Nome: <span>{usuario.nome}</span></p>
-            <p>Idade: <span>{usuario.idade}</span></p>
-            <p>Email: <span>{usuario.email}</span></p>
+          <div>
+            <p>Nome: {usuario.nome}</p>
+            <p>Idade: {usuario.idade}</p>
+            <p>Email: {usuario.email}</p>
           </div>
-          <button className='delete-btn'>
-            <img src={Lixeira} alt="Excluir" />
+          <button onClick={()=> deleteUsuarios(usuario.id)}>
+            <img src={Lixeira} alt="Deletar" style={{ width: '50px', height: '50px' }} />
           </button>
         </div>
       ))}
